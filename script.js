64f7e083239d3c861f76176fa10fd39ffe2c91ea
Thiago Duarte <104696554+ThiagoMDuarte@users.noreply.github.com>
2024-09-04 14:21:10 -0300

    Create 1 round play

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function getComputerChoice() {
     const max = 3
-    return Math.floor(Math.random() * max)
+    choice = Math.floor(Math.random() * max)
+    if (choice == 1) {
+        return 'pedra'
+    }
+    else if (choice == 0) {
+        return 'pedra'
+    } else {
+        return 'papel'
+    }
 }
 
 function getHumanChoice() {
@@ -8,3 +16,42 @@ function getHumanChoice() {
     choice = choice.toLowerCase()
     return choice
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+    // Dois iguas = empate
+    if (humanChoice === computerChoice) 
+        return console.log('Empate')
+
+    // pedra - papel = papel
+    else if (humanChoice == 'pedra' && computerChoice == 'papel')   
+        return console.log('Papel ganhou 1')
+
+    // pedra - tesoura = pedra
+    else if (humanChoice == 'pedra' && computerChoice == 'tesoura')
+        return console.log('Pedra ganhou 2')
+
+    // papel - tesoura = tesoura
+    else if (humanChoice == 'papel' && computerChoice == 'tesoura')
+        return console.log('Tseoura ganhou 3')
+
+    // papel - pedra = papel
+    else if (humanChoice == 'papel' && computerChoice == 'pedra')
+        return console.log('Papel ganhou 4')
+
+    // tesoura - papel = tesoura
+    else if (humanChoice == 'tesoura' && computerChoice == 'papel')
+        return console.log('Tesoura ganhou 5')
+
+    // tesoura - pedra = pedra
+    else if (humanChoice == 'tesoura' && computerChoice == 'pedra')
+        return console.log('Pedra ganhou 6')
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
